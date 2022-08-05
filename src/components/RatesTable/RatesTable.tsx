e57e288b81f6660388b5af0168f202b29feefd60
Yegor Kochetkov <kegor.fx@gmail.com>
2022-08-05 15:04:25 +0300

    Fix loader display in rates table

--- src/components/RatesTable/RatesTable.tsx
+++ src/components/RatesTable/RatesTable.tsx
@@ -9,43 +9,47 @@ import Paper from "@mui/material/Paper";
 import { useGetRatesQuery } from "../../redux/currenciesApi";
 import { useAppSelector } from "../../redux/hooks";
 import { selectBaseCurrency } from "../../redux/baseCurrencySlice";
+import { Loader } from "../Loader/Loader";
 
 export const RatesTable = () => {
   const baseCurrency = useAppSelector(selectBaseCurrency);
-  const { data } = useGetRatesQuery(baseCurrency);
+  const { data, isFetching } = useGetRatesQuery(baseCurrency);
   const rates = data && Object.entries(data.rates);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 260 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Currency</TableCell>
-            <TableCell align="right">Rate</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rates?.map((rate) => {
-            if (rate[0] !== baseCurrency) {
-              return (
-                <TableRow
-                  key={rate[0]}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {rate[0]}
-                  </TableCell>
-                  <TableCell component="th" scope="row" align="right">
-                    {rate[1].toFixed(4)}
-                  </TableCell>
-                </TableRow>
-              );
-            }
+    <>
+      {isFetching && <Loader />}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 260 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Currency</TableCell>
+              <TableCell align="right">Rate</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rates?.map((rate) => {
+              if (rate[0] !== baseCurrency) {
+                return (
+                  <TableRow
+                    key={rate[0]}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {rate[0]}
+                    </TableCell>
+                    <TableCell component="th" scope="row" align="right">
+                      {rate[1].toFixed(4)}
+                    </TableCell>
+                  </TableRow>
+                );
+              }
 
-            return null;
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              return null;
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
